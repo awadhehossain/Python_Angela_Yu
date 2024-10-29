28c228d8dc58d6c63641d285cc92c4ebd161d670
Awadhe Hossain <awadhehossain@Awadhes-MacBook-Pro-4.local>
2024-10-30 00:31:13 +0600

    Mistake

---
+++ Day 1/debugging.py
@@ -0,0 +1,15 @@
+# Fix the code below 👇
+
+print(Notes from Day 1")
+ print("The print statement is used to output strings")
+print("Strings are strings of characters"
+priint("String Concatenation is done with the + sign")
+print(("New lines can be created with a \ and the letter n")
+
+
+#solution
+print("Notes from Day 1") # SyntaxError: invalid syntax(need a double qoutes to identify where the string start and where it ends)
+print("The print statement is used to output strings")# Before start a line useing backspace ,tab gives error
+print("Strings are strings of characters")# Ending first braces
+print("String Concatenation is done with the + sign")#Print function spelling error
+print("New lines can be created with a \ and the letter n")# Extra first braces
