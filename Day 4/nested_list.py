5e6929e438e932b10959492570b17d2663cddf97
Awadhe Hossain <awadhehossain@Awadhes-MacBook-Pro-4.local>
2024-11-07 16:45:05 +0600

    Game

--- Day 4/nested_list.py
+++ Day 4/nested_list.py
@@ -3,6 +3,19 @@ vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
 dirty_dozen=[fruits,vegetables]
 print(dirty_dozen)
 
+# Nested list element print
+a=dirty_dozen[0][0]
+print(a) # fruits[0]
+b=dirty_dozen[1][4]
+print(b) # vegetables[4]
+c=dirty_dozen[-1][-5]
+print(c) # vegetables[0]
+d=dirty_dozen[-2][-6]
+print(d) # fruits[1]
+e=dirty_dozen[0][-1]
+print(e) # fruits[6]
+f=dirty_dozen[-2][5]
+print(f)
 
 
 
