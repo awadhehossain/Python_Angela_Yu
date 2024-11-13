751819a0a3e01a8bbe121fb80a310ac8afa5f9d7
awadhehossain <awadhehossain67@gmail.com>
2024-11-13 22:12:06 +0600

    New test

--- Day 1/band_name_generator_project.py
+++ Day 1/band_name_generator_project.py
@@ -3,5 +3,5 @@ city=input("Which city do you grow up in?\n")
 pet=input("What is the name of your pet?\n")
 print("Your band name could be "+city+" "+pet+".")
 
-#Fun
+# Another way
 print("Your band name could be "+input("Which city do you grow up in?\n")+" "+input("What is the name of your pet?\n")+".")
