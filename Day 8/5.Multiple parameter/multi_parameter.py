d206e9e2229a73c73b3657ff7b096af82012d12b
awadhehossain <awadhehossain67@gmail.com>
2025-03-11 00:43:22 +0600

    edit

--- Day 8/5.Multiple parameter/multi_parameter.py
+++ Day 8/5.Multiple parameter/multi_parameter.py
@@ -1,8 +1,11 @@
-def greet_name(name,location) :
+def my_function(name,location):
+    # This name and location is function parameter
     print(f"Hello {name}.")
-    print(f"What is it like in {location}?")
+    print(f"Is your location in {location}?")
 
-greet_name("Awadhe","Dhaka")
+name=input("What is your name?\n")
+location=input("What is your location?\n")
+# This name and location is variable which passes through the parameter both are not same thing 
+# Also the sequence should be maintained otherwise you dont get the actuall output
+my_function(name,location)
 
-greet_name("Dhaka","Awadhe")
-# Passing order is important
